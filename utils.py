94cf742e1774ffc4309142ea27ed8e149547699e
brain <brainbow30@gmail.com>
2020-12-22 16:17:27 +0000

    display predicted trajectories onto frame

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
+import math
+
+import networkx as nx
 import numpy as np
+import torch
 
 
 def to_image_frame(Hinv, loc):
@@ -20,3 +24,49 @@ def to_image_frame(Hinv, loc):
     if (np.array_equal(np.eye(3), Hinv)):
         imgCoord = np.flip(imgCoord)
     return imgCoord
+
+
+def centerCoord(coordArray):
+    coordArray = [float(x) for x in coordArray]
+    x_min, y_min, x_max, y_max = coordArray
+    return (x_min + x_max) / 2.0, (y_min + y_max) / 2.0
+
+
+def convertToRelativeSequence(sequence):
+    rel_curr_ped_seq = np.zeros(sequence.shape)
+    rel_curr_ped_seq[:, 1:] = \
+        sequence[:, 1:] - sequence[:, :-1]
+    return rel_curr_ped_seq
+
+
+def seq_to_graph(seq_, seq_rel, norm_lap_matr=True):
+    seq_ = seq_.squeeze()
+    seq_rel = seq_rel.squeeze()
+    seq_len = seq_.shape[2]
+    max_nodes = seq_.shape[0]
+
+    V = np.zeros((seq_len, max_nodes, 2))
+    A = np.zeros((seq_len, max_nodes, max_nodes))
+    for s in range(seq_len):
+        step_ = seq_[:, :, s]
+        step_rel = seq_rel[:, :, s]
+        for h in range(len(step_)):
+            V[s, h, :] = step_rel[h]
+            A[s, h, h] = 1
+            for k in range(h + 1, len(step_)):
+                l2_norm = anorm(step_rel[h], step_rel[k])
+                A[s, h, k] = l2_norm
+                A[s, k, h] = l2_norm
+        if norm_lap_matr:
+            G = nx.from_numpy_matrix(A[s, :, :])
+            A[s, :, :] = nx.normalized_laplacian_matrix(G).toarray()
+
+    return torch.from_numpy(V).type(torch.float), \
+           torch.from_numpy(A).type(torch.float)
+
+
+def anorm(p1, p2):
+    NORM = math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+    if NORM == 0:
+        return 0
+    return 1 / (NORM)
--- videoCamera.py
+++ videoCamera.py
@@ -2,9 +2,13 @@ import os
 
 import cv2
 import numpy as np
+import torch
+import torch.distributions.multivariate_normal as torchdist
 
+import metrics
 import utils
 from createAnnotations import annotations
+from model import social_stgcnn
 
 
 class VideoCamera(object):
@@ -17,26 +21,40 @@ class VideoCamera(object):
         self.annotations = annotations(self.path)
         homog_file = "annotations/" + self.path + "/H.txt"
         self.H = np.linalg.inv((np.loadtxt(homog_file))) if os.path.exists(homog_file) else np.eye(3)
+        self.model = social_stgcnn(n_stgcnn=1, n_txpcnn=5,
+                                   output_feat=5, seq_len=8,
+                                   kernel_size=3, pred_seq_len=12).cuda()
+        self.model.load_state_dict(
+            torch.load(
+                "C:\\Users\\brain\\PycharmProjects\\Social-STGCNN\\checkpoint\\social-stgcnn-hotel\\val_best.pth"))
+        self.model.cuda()
 
     def __del__(self):
         # releasing camera
         self.video.release()
-
-    def centerCoord(self, coordArray):
-        coordArray = [float(x) for x in coordArray]
-        x_min, y_min, x_max, y_max = coordArray
-        return (x_min + x_max) / 2.0, (y_min + y_max) / 2.0
 
     def get_frame(self, displayCircles=True):
         # extracting frames
         ret, frame = self.video.read()
         annotations = self.annotations.getFrameAnnotations(self.video.get(cv2.CAP_PROP_POS_FRAMES))
         newPedPastTraj = {}
+        keys = list(self.pedPastTraj.keys())
+        # plot tracking circles and update past trajectories
         for annotation in annotations:
             if (displayCircles):
                 self.displayAnnotation(frame, annotation)
             self.updatePastTraj(annotation, newPedPastTraj)
         self.pedPastTraj = newPedPastTraj
+
+        # predict trajectories
+        predTrajectories = self.predict()
+        for framePrediction in predTrajectories:
+            for i in range(len(framePrediction)):
+                predX, predY = framePrediction[i]
+                pos = [predX, predY]
+                y, x = utils.to_image_frame(self.H, np.array(pos))
+                cv2.circle(frame, center=(x, y), radius=3, color=self.colours[keys[i]], thickness=2)
+
         ret, jpeg = cv2.imencode('.jpg', frame)
         return ret, jpeg.tobytes()
 
@@ -49,7 +67,7 @@ class VideoCamera(object):
         if (not (ped_id in self.colours)):
             self.colours[ped_id] = (int(np.random.randint(0, 255)), int(np.random.randint(0, 255)),
                                     int(np.random.randint(0, 255)))
-        centerX, centerY = self.centerCoord([x_min, y_min, x_max, y_max])
+        centerX, centerY = utils.centerCoord([x_min, y_min, x_max, y_max])
         centerX, centerY = int(centerX), int(centerY)
         if (eval(label) == "Pedestrian"):
             cv2.circle(frame, center=(centerX, centerY), radius=7, color=self.colours[ped_id],
@@ -70,3 +88,69 @@ class VideoCamera(object):
         else:
             newPedPastTraj[ped_id] = []
         newPedPastTraj[ped_id].append([x_min, y_min, x_max, y_max])
+
+    def predict(self, keys=None, samples=20):
+        pedPastTraj = self.pedPastTraj
+        if (keys is None):
+            pedPastTraj = dict(filter(lambda elem: len(elem[1]) == 8, pedPastTraj.items()))
+        else:
+            pedPastTraj = dict(filter(lambda elem: elem[0] in keys and len(elem[1]) == 8, pedPastTraj.items()))
+        if (len(pedPastTraj) > 2):
+            seq_list = []
+            for key in sorted(list(pedPastTraj.keys())):
+                pedestrianSeq = pedPastTraj[key]
+                xcoords = []
+                ycoords = []
+                for coords in pedestrianSeq:
+                    x, y = utils.centerCoord(coords)
+
+                    xcoords.append(x)
+                    ycoords.append(y)
+                seq_list.append([np.array([xcoords, ycoords])])
+            seq_list = np.concatenate(seq_list, axis=0)
+            seq_list_rel = utils.convertToRelativeSequence(seq_list)
+            obs_traj = torch.from_numpy(
+                seq_list).type(torch.float)
+            obs_traj_rel = torch.from_numpy(
+                seq_list_rel).type(torch.float)
+            V_obs = []
+            A_obs = []
+            v_, a_ = utils.seq_to_graph(obs_traj, obs_traj_rel, True)
+
+            V_obs.append(v_.clone())
+            A_obs.append(a_.clone())
+            V_obs = torch.stack(V_obs).cuda()
+            A_obs = torch.stack(A_obs).cuda()
+            V_pred, _ = self.model(V_obs.permute(0, 3, 1, 2), A_obs.squeeze())
+            V_pred = V_pred.permute(0, 2, 3, 1)
+            V_pred = V_pred.squeeze()
+            num_of_objs = obs_traj_rel.shape[1]
+            # V_pred = V_pred[:, :num_of_objs, :]
+
+            V_x = metrics.seq_to_nodes(obs_traj.data.cpu().numpy().copy())
+
+            sx = torch.exp(V_pred[:, :, 2])  # sx
+            sy = torch.exp(V_pred[:, :, 3])  # sy
+            corr = torch.tanh(V_pred[:, :, 4])  # corr
+
+            cov = torch.zeros(V_pred.shape[0], V_pred.shape[1], 2, 2).cuda()
+            cov[:, :, 0, 0] = sx * sx
+            cov[:, :, 0, 1] = corr * sx * sy
+            cov[:, :, 1, 0] = corr * sx * sy
+            cov[:, :, 1, 1] = sy * sy
+            mean = V_pred[:, :, 0:2]
+
+            # create a guassian distribution and sample it
+            mvnormal = torchdist.MultivariateNormal(mean, cov)
+            # shape 12 - frames, n - pedestrians, 2 - x,y
+            trajectories = []
+            # take multiple samples and average
+            for i in range(samples):
+                V_pred = mvnormal.sample()
+                # take predictions and add the pedestrians start pos to find actual position
+                trajectories.append(metrics.nodes_rel_to_nodes_abs(V_pred.data.cpu().numpy().squeeze().copy(),
+                                                                   V_x[-1, :, :].copy()))
+            return np.average(trajectories, axis=0)
+
+        else:
+            return []
